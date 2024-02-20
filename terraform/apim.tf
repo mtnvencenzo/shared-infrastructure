85ce4c430ae2c3b591abf5248297b10e19dbf811
Ronaldo Vecchi <rvecchi@gmail.com>
2024-02-20 09:22:52 -0500

    removing logger

--- terraform/apim.tf
+++ terraform/apim.tf
@@ -12,15 +12,3 @@ resource "azurerm_api_management" "apim" {
     prevent_destroy = true
   }
 }
-
-resource "azurerm_api_management_logger" "apim_appinsights_logger" {
-  name                = "shared-appinsights-logger"
-  description         = "Shared app insights logger for the entire api management instance"
-  api_management_name = azurerm_api_management.apim.name
-  resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
-  resource_id         = azurerm_application_insights.appi.id
-
-  application_insights {
-    instrumentation_key = azurerm_application_insights.appi.instrumentation_key
-  }
-}
