104871bce3c1a52c23b2879b7c8bcde5c9877eca
Ronaldo Vecchi <rvecchi@gmail.com>
2024-03-03 13:40:05 -0500

    adding shared cdn

---
+++ terraform/cdn.tf
@@ -0,0 +1,7 @@
+resource "azurerm_cdn_profile" "cdn" {
+  name                  = "cdn-${var.sub}-${var.region}-${var.environment}-${var.domain}-${var.sequence}"
+  resource_group_name   = data.azurerm_resource_group.global_shared_resource_group.name
+  location              = data.azurerm_resource_group.global_shared_resource_group.location
+  sku                   = "Standard Verizon"
+  tags                  = local.tags
+}
