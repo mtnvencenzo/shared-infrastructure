d9457dcb197813d53ebb3c54af2a680c7842262c
Ronaldo Vecchi <rvecchi@gmail.com>
2024-03-03 13:42:24 -0500

    updates

--- terraform/cdn.tf
+++ terraform/cdn.tf
@@ -2,6 +2,6 @@ resource "azurerm_cdn_profile" "cdn" {
   name                  = "cdn-${var.sub}-${var.region}-${var.environment}-${var.domain}-${var.sequence}"
   resource_group_name   = data.azurerm_resource_group.global_shared_resource_group.name
   location              = data.azurerm_resource_group.global_shared_resource_group.location
-  sku                   = "Standard Verizon"
+  sku                   = "Standard_Verizon"
   tags                  = local.tags
 }
