6157861749c0f53683bc5e4531f0fe2dfe3d4fa8
Ronaldo Vecchi <rvecchi@gmail.com>
2024-07-19 05:37:27 -0600

    updates

--- terraform/servicebus.tf
+++ terraform/servicebus.tf
@@ -1,13 +1,16 @@
 module "shared_servicebus_namespace" {
     source                  = "git::https://vecchi@dev.azure.com/vecchi/Latest/_git/Terraform-Modules//modules/servicebus-namespace"
     
-    resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
-    location            = data.azurerm_resource_group.global_shared_resource_group.location
+    resource_group_name             = data.azurerm_resource_group.global_shared_resource_group.name
+    location                        = data.azurerm_resource_group.global_shared_resource_group.location
+    sub                             = var.sub
+    region                          = var.region
+    environment                     = var.environment
+    domain                          = var.domain
 
-    sub                     = var.sub
-    region                  = var.region
-    environment             = var.environment
-    domain                  = var.domain
+    public_network_access_enabled   = true
+    local_auth_enabled              = false
+    prevent_destroy                 = false
 
     providers = {
         azurerm = azurerm
