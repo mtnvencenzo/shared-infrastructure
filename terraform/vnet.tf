4b1298ed6187b20cbd5254471eef6d0466847754
Ronaldo Vecchi <rvecchi@gmail.com>
2025-01-24 02:35:07 +0000

    Merged PR 249: Expanding vnet
    Related work items: #304

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "global_vnet" {
   name                = "vnet-${var.sub}-${var.region}-${var.environment}-network-${var.sequence}"
   location            = data.azurerm_resource_group.global_network_resource_group.location
   resource_group_name = data.azurerm_resource_group.global_network_resource_group.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/8"]
   tags     = local.tags
   lifecycle {
     prevent_destroy = true
