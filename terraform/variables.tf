ee4b42b767833fd0aa1780e7bab9da521907a967
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-01 10:32:44 -0700

    updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -67,5 +67,5 @@ variable "log_analytics_workspace_tables_for_reduced_retention_period" {
 variable "log_analytics_workspace_tables_reduced_retention_period" {
   type = number
   description = "The number of days to use for the retention period"
-  default = 30
+  default = 90 # cant be less than 90 (which is the default so this is kind of not needed)
 }
