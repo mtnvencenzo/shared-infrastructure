857abb943c44d1b0e52dab467fc22bf48aa23d3a
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-17 12:58:32 -0700

    moving global cocktails keyvault

--- .deployment/iac/terraform/cezzis_shared_keyvault.tf
+++ .deployment/iac/terraform/cezzis_shared_keyvault.tf
@@ -1,4 +1,4 @@
-module "keyvault" {
+module "cezzis_keyvault" {
     source  = "git::https://vecchi@dev.azure.com/vecchi/Latest/_git/Terraform-Modules//modules/key-vault"
     providers = {
         azurerm = azurerm
@@ -7,14 +7,15 @@ module "keyvault" {
     sub                             = var.sub
     region                          = var.region
     environment                     = var.environment
-    domain                          = var.domain
-    shortdomain                     = var.shortdomain
+    domain                          = "cocktails"
+    shortdomain                     = "cockti"
     sequence                        = var.short_sequence
     tenant_id                       = data.azurerm_client_config.current.tenant_id
     pipeline_object_id              = data.azurerm_client_config.current.object_id
-    resource_group_name             = data.azurerm_resource_group.global_shared_resource_group.name
-    resource_group_location         = data.azurerm_resource_group.global_shared_resource_group.location
+    resource_group_name             = data.azurerm_resource_group.cocktails_glo_resource_group.name
+    resource_group_location         = data.azurerm_resource_group.cocktails_glo_resource_group.location
     
-    virtual_network_subnet_ids = []
-    secrets = []
+    virtual_network_subnet_ids  = []
+    secrets                     = []
+    secrets_values_ignored      = []
 }
