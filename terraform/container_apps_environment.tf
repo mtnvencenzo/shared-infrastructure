30b6c260a368185fc22ee8d77e967d4474fc6e19
mtnvencenzo <rvecchi@gmail.com>
2025-09-07 18:11:00 -0600

    fixes

--- terraform/container_apps_environment.tf
+++ terraform/container_apps_environment.tf
@@ -27,7 +27,6 @@ resource "azapi_resource_action" "open_telemetry_config" {
   type        = "Microsoft.App/managedEnvironments@2025-02-02-preview"
   method      = "PUT"
   location    = data.azurerm_resource_group.global_shared_resource_group.location
-  tags        = local.tags
   resource_id = azurerm_container_app_environment.container_app_environment.id
 
   body = {
