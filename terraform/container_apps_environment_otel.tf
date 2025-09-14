eaf8ce7fd7d97fceff0c4a07f98fffe3a51b5a7d
mtnvencenzo <rvecchi@gmail.com>
2025-09-14 12:55:38 -0600

    fix (otel): Fixing terraform deploy issue for otel collector

--- terraform/container_apps_environment_otel.tf
+++ terraform/container_apps_environment_otel.tf
@@ -171,7 +171,6 @@ resource "azurerm_container_app" "otel_collector" {
   ingress {
     external_enabled = true
     target_port      = 4318
-    exposed_port     = 4318
     transport        = "http"
 
     traffic_weight {
