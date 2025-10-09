245e29d57b8687496e2517ac053bbbdceb8c4b88
Ronaldo Vecchi <rvecchi@gmail.com>
2025-10-09 17:21:58 -0600

    chore (acr): Testing import of acr (#19)
    * chore (acr): Testing import of acr
    
    * fixes

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -1,24 +1,3 @@
-# resource "azurerm_container_registry" "acr" {
-#   name                  = "acr${var.sub}${var.region}${var.environment}${var.domain}${var.sequence}"
-#   resource_group_name   = data.azurerm_resource_group.global_shared_resource_group.name
-#   location              = data.azurerm_resource_group.global_shared_resource_group.location
-#   sku                   = "Basic"
-#   admin_enabled         = true
-#   data_endpoint_enabled = false
-#   identity {
-#     type = "SystemAssigned"
-#   }
-#   tags = {
-#     Environment = "${var.environment}"
-#     Application = "${var.domain}"
-#   }
-#   lifecycle {
-#     prevent_destroy = true
-#     ignore_changes = [
-#       tags
-#     ]
-#   }
-# }
 
 module "acr" {
   source = "git::ssh://git@github.com/mtnvencenzo/terraform-modules.git//modules/container-registry"
@@ -28,12 +7,15 @@ module "acr" {
 
   resource_group_name     = data.azurerm_resource_group.global_shared_resource_group.name
   resource_group_location = data.azurerm_resource_group.global_shared_resource_group.location
+  domain                  = var.domain
+  environment             = var.environment
 
-  domain      = var.domain
-  environment = var.environment
+  sku                   = "Basic"
+  admin_enabled         = true
+  data_endpoint_enabled = false
 }
 
-import {
-  to = module.acr.azurerm_container_registry.acr
-  id = "/subscriptions/1d9ecc00-242a-460d-8b08-b71db19f094e/resourceGroups/rg-vec-eus-glo-shared-001/providers/Microsoft.ContainerRegistry/registries/acrveceusgloshared001"
-}
+# import {
+#   to = module.acr.azurerm_container_registry.acr
+#   id = "/subscriptions/1d9ecc00-242a-460d-8b08-b71db19f094e/resourceGroups/rg-vec-eus-glo-shared-001/providers/Microsoft.ContainerRegistry/registries/acrveceusgloshared001"
+# }
