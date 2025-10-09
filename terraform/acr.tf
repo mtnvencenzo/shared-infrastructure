f4e60c319be38f1904c8a1645878587e96e7aad3
mtnvencenzo <rvecchi@gmail.com>
2025-10-09 16:51:27 -0600

    test (acr): Attempting import of acr

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -25,6 +25,13 @@ module "acr" {
   providers = {
     azurerm = azurerm
   }
+
+  name                = "acr${var.sub}${var.region}${var.environment}${var.domain}${var.sequence}"
+  resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
+  location            = data.azurerm_resource_group.global_shared_resource_group.location
+
+  domain      = var.global_domain
+  environment = var.global_environment
 }
 
 import {
