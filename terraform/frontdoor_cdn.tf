9119a7e181441c787d68e715351abe4a0b12bd12
mtnvencenzo <rvecchi@gmail.com>
2026-03-01 17:16:11 -0700

    feat(k8s): adding service principal for k8s

--- terraform/frontdoor_cdn.tf
+++ terraform/frontdoor_cdn.tf
@@ -1,6 +1,6 @@
-resource "azurerm_cdn_frontdoor_profile" "afd" {
-  name                = "afd-${var.sub}-${var.region}-${var.environment}-${var.domain}-${var.sequence}"
-  resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
-  sku_name            = "Standard_AzureFrontDoor"
-  tags                = local.tags
-}
+# resource "azurerm_cdn_frontdoor_profile" "afd" {
+#   name                = "afd-${var.sub}-${var.region}-${var.environment}-${var.domain}-${var.sequence}"
+#   resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
+#   sku_name            = "Standard_AzureFrontDoor"
+#   tags                = local.tags
+# }
--- terraform/monitor.tf
+++ terraform/monitor.tf
@@ -1,10 +1,10 @@
-resource "azurerm_monitor_action_group" "team_vecchi_action_group" {
-  name                = "ag-teamvecchi-${var.sub}-${var.region}-${var.environment}-${var.domain}"
-  resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
-  short_name          = "teamvec"
+# resource "azurerm_monitor_action_group" "team_vecchi_action_group" {
+#   name                = "ag-teamvecchi-${var.sub}-${var.region}-${var.environment}-${var.domain}"
+#   resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
+#   short_name          = "teamvec"
 
-  email_receiver {
-    name          = "Ron Vecchi"
-    email_address = "rvecchi+azure@gmail.com"
-  }
-}
+#   email_receiver {
+#     name          = "Ron Vecchi"
+#     email_address = "rvecchi+azure@gmail.com"
+#   }
+# }
